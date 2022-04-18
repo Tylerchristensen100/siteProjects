d7fa2d9c2c2ce4a464acdd455ed5c2a6c61a1428
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-04-18 14:53:24 -0600

    delete

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -4,9 +4,15 @@ import {BiCalendar,BiTrash } from 'react-icons/bi';
 import Search from './components/Search';
 import AddAppointment from './components/AddAppointment';
 import AppointmentInfo from './components/AppointmentInfo';
-import appointmentList from './data.json';
+
+import {useState, useEffect, useCallback } from 'react';
 
 function App() {
+let {appointmentList, setAppointmentList} = useState([]);
+const fetchData = useCallback(() => {
+  fetch('./data.json').then(response => response.json()).then(data => {setAppointmentList(data)});
+});
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl"><BiCalendar className="inline-block text-red-400 text-8xl" />Appointments</h1>
