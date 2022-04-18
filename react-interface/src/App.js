481126e4eac9d509e1c74dfa0a7435303f6a0fd8
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-04-18 14:01:39 -0600

    added components

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {BiArchive } from 'react-icons/bi';
+import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <h1 className="text-5xl">Appointments</h1>
 
       <BiArchive className="inline-block text-red-400 text-8xl" />
+      <AddAppointment />
+      <Search />
     </div>
   );
 }
