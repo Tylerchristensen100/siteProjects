7339545bb10d1efdb3866dd7dbdc064be553d182
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-04-20 11:09:55 -0600

    client and server

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Routes, Route, } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
+import './App.css';
 
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/articles" component={ArticlesList} />
-        <Route path="/article" component={ArticlePage} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <div id="page-body">
+          <Routes>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} />
+            </Routes>
+          </div>
+        </div>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
