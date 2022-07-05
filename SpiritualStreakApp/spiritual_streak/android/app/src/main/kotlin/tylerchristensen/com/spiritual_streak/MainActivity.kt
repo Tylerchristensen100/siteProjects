4ff739442afa8e932ffaab4ff4ed38a70082b759
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-07-05 10:19:52 -0600

    vue

--- SpiritualStreakApp/spiritual_streak/android/app/src/main/kotlin/tylerchristensen/com/spiritual_streak/MainActivity.kt
+++ SpiritualStreakApp/spiritual_streak/android/app/src/main/kotlin/tylerchristensen/com/spiritual_streak/MainActivity.kt
@@ -2,5 +2,4 @@ package tylerchristensen.com.spiritual_streak
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
