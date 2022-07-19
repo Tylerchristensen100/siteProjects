bc211703be3229dde229b757d685513b5f185a70
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-07-19 10:39:10 -0600

    customized

--- Vue Training/Calculator/src/router/index.ts
+++ Vue Training/Calculator/src/router/index.ts
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import calculator from "../views/calculator.vue";
+import login from "../views/login.vue";
+import register from "../views/register.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "calculator",
+      component: calculator,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/login",
+      name: "login",
+      component: login,
     },
+    {
+      path: "/register",
+      name: "register",
+      component: register,
+    }
+    
   ],
 });
 
---
+++ Vue Training/Calculator/src/views/calculator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="calculator">
+    <h1>Calculator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator'
+}
+</script>
