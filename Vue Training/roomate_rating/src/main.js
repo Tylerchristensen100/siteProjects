4e391fb0fea32e6dbddf7fa05454bdeb4587d655
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-08-01 09:42:04 -0600

    rewstarting...

--- Vue Training/roomate_rating/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { firestorePlugin } from 'vuefire'
-
-Vue.use(firestorePlugin)
-
-createApp(App).use(router).mount('#app')
