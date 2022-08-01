e24231b19918f0baf033f59d347be9fda16e51c1
Tyler Christensen <31700358+Tylerchristensen100@users.noreply.github.com>
2022-08-01 08:54:52 -0600

    rebuild of roomate rating

---
+++ Vue Training/roomate_rating/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
+
+createApp(App).use(router).mount('#app')
